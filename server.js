3908322592dbf22d77a9bb7abfe4cb7983e44fdf
temidayo <temidayooyedele@yahoo.com>
2018-01-10 10:25:23 +0100

    setup production webpack

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ import webpackMiddleware from 'webpack-dev-middleware';
 import UserRouter from './server/routes/users';
 import BookRouter from './server/routes/books';
 import webpackConfig from './webpack.config';
+import webpackProd from './webpack.config.prod';
 
 dotenv.load();
 const app = express();
@@ -48,6 +49,8 @@ const swaggerSpec = swagger(options);
 app.use(logger('dev'));
 if(process.env.NODE_ENV !== 'production'){
   app.use(webpackMiddleware(webpack(webpackConfig)));
+}else{
+  app.use(webpackMiddleware(webpack(webpackProd)));
 }
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
