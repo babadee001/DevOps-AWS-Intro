fb95f33951fd9f0e70c0ea06629b8669e3b482a7
temidayo <temidayooyedele@yahoo.com>
2018-01-10 13:11:49 +0100

    setup production webpack

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,16 +1,34 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   devtool: 'source-map',
   plugins: [
-    new UglifyJSPlugin({
-      sourceMap: true
-    }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new CleanWebpackPlugin(['client/']),
+    new webpack.EnvironmentPlugin([
+      'FIREABSE_DOMAIN',
+      'FIREBASE_MESSENGERID',
+      'FIREBASE_APIKEY',
+      'FIREBASE_URL',
+      'FIREBASE_PROJECTID',
+      'FIREBASE_STORAGEBUCKET'
+    ]),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.LoaderOptionsPlugin({
+      debug: false
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      output: {
+        comments: false
+      }
     })
   ]
 });
