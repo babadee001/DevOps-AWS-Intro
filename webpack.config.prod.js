d75eac4fce433ecc5c5a8d1f344ad91a8118e454
temidayo <temidayooyedele@yahoo.com>
2018-01-10 15:27:19 +0100

    setup production webpack

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   cache: true,
