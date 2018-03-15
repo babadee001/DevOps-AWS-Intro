1213783632435e6fa21413a1b8b3e3cfc3493ec3
babadee001 <temidayooyedele300@gmail.com>
2018-03-15 06:19:48 +0100

    setup PM2 ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [{
+    name: 'HelloBooks',
+    script: './app.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-18-188-75-227.us-east-2.compute.amazonaws.com',
+      key: '~/.ssh/HelloBooksKey.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:babadee001/DevOps-AWS-Intro.git',
+      path: '/home/ubuntu/HelloBooks',
+      'post-deploy': 'npm install && npm start && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+};
+
