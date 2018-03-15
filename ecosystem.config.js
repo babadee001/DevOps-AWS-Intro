4a97207f486ba9aa01e983563fd34d1d1b65fa5d
babadee001 <temidayooyedele300@gmail.com>
2018-03-15 16:06:13 +0100

    modify pm2 script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
       ref: 'origin/master',
       repo: 'git@github.com:babadee001/DevOps-AWS-Intro.git',
       path: '/home/ubuntu/HelloBooks',
-      'post-deploy': 'npm install; npm run build; npm start; pm2 startOrRestart ecosystem.config.js'
+      'post-deploy': 'npm install; npm run build; pm2 start npm -- start ecosystem.config.js'
     }
   }
 };
