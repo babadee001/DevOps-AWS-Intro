7908eac22966d64b8d8f7fd2bf1f68396b6aa061
babadee001 <temidayooyedele300@gmail.com>
2018-03-19 12:08:13 +0100

    add database URL

--- server/models/index.js
+++ server/models/index.js
@@ -11,10 +11,10 @@ const db = {};
 dotenv.config();
 let sequelize;
 if (config.use_env_variable) {
-  console.log(process.env[config.use_env_variable]);
+  console.log('prod', env, process.env[config.use_env_variable]);
   sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
-  console.log(process.env[config.use_env_variable]);
+  console.log('dev', env, process.env[config.use_env_variable]);
   sequelize = new Sequelize(
     config.database, config.username, config.password, config
   );
