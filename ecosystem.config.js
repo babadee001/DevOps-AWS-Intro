38baff88656490dd81783efd4440eb6ddb5995da
babadee001 <temidayooyedele300@gmail.com>
2018-03-19 09:13:09 +0100

    add database URL

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,7 @@ module.exports = {
     name: 'HelloBooks',
     script: 'app.js',
     "environment_variables" : {
-      NODE_ENV: 'production',
-      DATABASE_URL: 'postgres://boddocvovmdlvc:1edf249c9462951822a551043e77809aa6b5342a37ae6598e7b52c826fab10cd@ec2-54-235-240-126.compute-1.amazonaws.com:5432/dch1ud2dv6hni1'
+      NODE_ENV: 'production'
     }
   },
 ],
@@ -16,7 +15,7 @@ module.exports = {
       ref: 'origin/master',
       repo: 'git@github.com:babadee001/DevOps-AWS-Intro.git',
       path: '/home/ubuntu/HelloBooks',
-      'post-deploy': 'npm install; npm run build; pm2 start app.js --interpreter ./node_modules/.bin/babel-node'
+      'post-deploy': 'npm install; npm run build; pm2 start --interpreter ./node_modules/.bin/babel-node'
     }
   }
 };
