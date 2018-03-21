6e634155a037c9445f91890c709a424b81d9fc4b
babadee001 <temidayooyedele300@gmail.com>
2018-03-21 15:00:11 +0100

    add automation start script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,7 +13,7 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-18-188-113-37.us-east-2.compute.amazonaws.com',
+      host: 'ec2-18-188-78-243.us-east-2.compute.amazonaws.com',
       ref: 'origin/master',
       repo: 'https://github.com/babadee001/DevOps-AWS-Intro',
       path: '/home/ubuntu/HelloBooks',
