a2ed8f75c5351ec2984ba14b7820f97125cea862
babadee001 <temidayooyedele300@gmail.com>
2018-04-07 11:16:23 +0100

    change host and db url

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,15 +6,15 @@ module.exports = {
     env: {
       NODE_ENV: 'production',
       dialect: 'postgres',
-      DATABASE_URL: 'postgres://lsfovzcr:u7rLpV5I0h3WTQLPXBDrfBICTapoe1o5@babar.elephantsql.com:5432/lsfovzcr'
+      DATABASE_URL: 'postgres://postgres:devops@45.2.2.196:5432/posgres'
     }
   },
   ],
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-18-188-78-243.us-east-2.compute.amazonaws.com',
-      key: '~/.ssh/HelloBooksKey.pem',
+      host: '45.2.2.196',
+      key: '~/.ssh/london.pem',
       ref: 'origin/master',
       repo: 'https://github.com/babadee001/DevOps-AWS-Intro',
       path: '/home/ubuntu/HelloBooks',
--- start.sh
+++ start.sh
@@ -1,26 +1,60 @@
 #!/bin/bash
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
-# source ~/.bashrc
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-#install node version 8.
-nvm install 8.9.4
-#pm2 install
-npm install -g pm2
-git clone https://github.com/babadee001/DevOps-AWS-Intro
-sudo apt-get install nginx
-sudo bash -c 'cat > /etc/nginx/sites-available/HelloBooks <<EOF
-server {
-  listen 80;
-  server_name HelloBooks;
-  location / {
-    proxy_set_header  X-Real-IP  $remote_addr;
-    proxy_set_header  Host       $http_host;
-    proxy_pass        http://18.188.78.243:8000;
-  }
-} 
-EOF'
-sudo ln -s /etc/nginx/sites-available/HelloBooks /etc/nginx/sites-enabled/HelloBooks
-sudo service nginx restart
-cd DevOps-AWS-Intro
-pm2 startOrRestart ecosystem.config.js
+echo ' ####################################### Starting script... #########################################'
+echo 'Enter your instance public IP'
+read ip
+echo 'Enter your server name like this ===> example.com'
+read serverName
+
+nodeSetup(){
+    echo '############################### Settting up Node Environment ################################'
+    curl -sL https://deb.nodesource.com/setup_8.x -o nodesource_setup.sh
+    sudo bash nodesource_setup.sh
+    sudo apt-get install nodejs -y
+    sudo npm install babel-cli -g
+}
+
+nginxSetup(){
+    echo '############################## Setting up pm2, nginx and getting the repo ########################################'
+    sudo npm install pm2 -g -y
+    git clone https://github.com/babadee001/DevOps-AWS-Intro
+    sudo apt-get install nginx
+    cd /etc/nginx/sites-enabled/default
+    sudo rm /etc/nginx/sites-enabled/default
+    sudo bash -c 'cat > /etc/nginx/sites-available/HelloBooks <<EOF
+    server {
+            listen 80;
+            server_name '$serverName' 'www.$serverName';
+            location / {
+                    proxy_pass 'http://$ip:8000';
+            }
+    }
+    EOF'
+    sudo ln -s /etc/nginx/sites-available/HelloBooks /etc/nginx/sites-enabled/HelloBooks
+    sudo /etc/init.d/nginx start
+    sudo /etc/init.d/nginx restart
+}
+
+installDep(){
+    echo '######################### Installing App Dependencies ##################################'
+    cd DevOps-AWS-Intro
+    sudo npm install --unsafe-perm
+}
+
+sslSetup(){
+    echo '############################### Setting up SSL Certificate ##############################'
+    sudo add-apt-repository ppa:certbot/certbot
+    sudo apt-get update
+    sudo apt-get install python-certbot-nginx
+    sudo certbot --nginx -d $serverName -d www.$serverName
+}
+
+startPm2() {
+    echo '############################### Starting App with pm2 ###############################'
+     pm2 startOrRestart ecosystem.config.js
+}
+
+nodeSetup
+nginxSetup
+installDep
+sslSetup
+startPm2
